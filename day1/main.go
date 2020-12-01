b99d7ae002666922cfac2b3ef45c421776a64623
Ujwal Dhakal <kevin.ujwal@gmail.com>
2020-12-02 00:06:49 +0545

    Updated two puzzle for day 1

--- day1/main.go
+++ day1/main.go
@@ -11,25 +11,51 @@ func main()  {
 
   inputs := loadInput()
 
-  firstNum, secondNum := solveChallange(inputs)
-  result := firstNum * secondNum
-  fmt.Println("the result is ",result)
+  firstNum, secondNum := solveChallange1(inputs)
+  fmt.Println("the result for 1st puzzle is ",firstNum * secondNum)
+  firstNum, secondNum,thirdNum := solveChallange2(inputs)
+  fmt.Println("the result for third puzzle is ",firstNum * secondNum * thirdNum)
 
 
 }
 
-func solveChallange(inputs []int) (int,int) {
+/**
+
+Search two number to form a single given num
+data [6,2,3,4]
+e.g 10 = ^+4
+ */
+func searchTwoNumComposition(inputs []int, numberToSearch int) (int,int) {
   for _,s := range inputs {
-    remainingHalf := 2020 - s
+    remainingHalf := numberToSearch - s
 
 
     if(itemExists(inputs,remainingHalf)) {
 
       return s,remainingHalf
     }
-
   }
   return 0,0
+}
+func solveChallange2(inputs []int) (int,int,int) {
+  for _,sampleCase := range inputs {
+    remainingHalf := 2020 - sampleCase
+
+    if(!itemExists(inputs,remainingHalf)) {
+
+      firstNum,secondNum := searchTwoNumComposition(inputs,remainingHalf)
+      if firstNum > 0 && secondNum > 0 {
+        return firstNum,secondNum,sampleCase
+      }
+    }
+
+  }
+  return 0,0,0
+}
+
+
+func solveChallange1(inputs []int) (int,int) {
+  return searchTwoNumComposition(inputs,2020)
 }
 
 
